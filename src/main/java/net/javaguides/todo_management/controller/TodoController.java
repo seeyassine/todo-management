1bcc12e0fc12484533180c3d54f8a8cbf3dd2099
See Yassine <seeyassine@gmail.com>
2026-01-29 00:54:25 +0100

    Build Add TodoREST API

---
+++ src/main/java/net/javaguides/todo_management/controller/TodoController.java
@@ -0,0 +1,25 @@
+package net.javaguides.todo_management.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.todo_management.dto.TodoDto;
+import net.javaguides.todo_management.service.TodoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/todos")
+@AllArgsConstructor
+public class TodoController {
+
+   private TodoService todoService;
+
+   // Build Add Todo REST API
+    @PostMapping
+    public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
+
+        TodoDto savedTodo = todoService.addTodo(todoDto);
+
+        return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
+    }
+}
