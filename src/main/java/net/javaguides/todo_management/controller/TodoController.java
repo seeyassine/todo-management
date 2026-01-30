156d91a7f9963003f4ebd1697a00cbd0baafe17b
See Yassine <seeyassine@gmail.com>
2026-01-30 03:30:11 +0100

    Build update todo Rest API

--- src/main/java/net/javaguides/todo_management/controller/TodoController.java
+++ src/main/java/net/javaguides/todo_management/controller/TodoController.java
@@ -39,4 +39,11 @@ public class TodoController {
 //       return new ResponseEntity<>(todos, HttpStatus.OK);
         return ResponseEntity.ok(todos);
     }
+
+    // Build update todo Rest API
+    @PutMapping("{id}")
+    public ResponseEntity<TodoDto> updateTodo(@RequestBody TodoDto todoDto, @PathVariable("id") Long todoId){
+        TodoDto updateTodo= todoService.updateTodo(todoDto, todoId);
+        return  ResponseEntity.ok(updateTodo);
+    }
 }
