07f634db345113f5fcebde72cf819e85300a0a45
See Yassine <seeyassine@gmail.com>
2026-02-02 22:39:39 +0100

    Configure JWT in Spring Security Config

--- src/main/java/net/javaguides/todo_management/config/SpringSecurityConfig.java
+++ src/main/java/net/javaguides/todo_management/config/SpringSecurityConfig.java
@@ -1,6 +1,8 @@
 package net.javaguides.todo_management.config;
 
 import lombok.AllArgsConstructor;
+import net.javaguides.todo_management.security.JwtAuthenticationEntryPoint;
+import net.javaguides.todo_management.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -26,6 +29,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfig {
 
     private UserDetailsService userDetailsService;
+
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+
+    private JwtAuthenticationFilter authenticationFilter;
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
@@ -46,6 +53,12 @@ public class SpringSecurityConfig {
                     authorize.requestMatchers("api/auth/**").permitAll();
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
+
+        http.exceptionHandling( exception -> exception
+                .authenticationEntryPoint(authenticationEntryPoint));
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return  http.build();
     }
 
