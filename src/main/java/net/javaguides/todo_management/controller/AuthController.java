d3144471f6d905fd58eb76c0796bc798dfff3265
See Yassine <seeyassine@gmail.com>
2026-02-02 03:52:31 +0100

    complated Build Login Rest API

--- src/main/java/net/javaguides/todo_management/controller/AuthController.java
+++ src/main/java/net/javaguides/todo_management/controller/AuthController.java
@@ -1,6 +1,7 @@
 package net.javaguides.todo_management.controller;
 
 import lombok.AllArgsConstructor;
+import net.javaguides.todo_management.dto.LoginDto;
 import net.javaguides.todo_management.dto.RegisterDto;
 import net.javaguides.todo_management.service.AuthService;
 import org.springframework.http.HttpStatus;
@@ -22,5 +23,12 @@ public class AuthController {
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
       String response =  authService.register(registerDto);
       return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    // BUILD Login REST API
+    @PostMapping("/login")
+    public  ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+        String response = authService.login(loginDto);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
--- src/main/java/net/javaguides/todo_management/service/AuthService.java
+++ src/main/java/net/javaguides/todo_management/service/AuthService.java
@@ -1,8 +1,11 @@
 package net.javaguides.todo_management.service;
 
+import net.javaguides.todo_management.dto.LoginDto;
 import net.javaguides.todo_management.dto.RegisterDto;
 
 public interface AuthService {
 
     String register(RegisterDto registerDto);
+
+    String login(LoginDto loginDto);
 }
